870c660d5a5e1b1ee268fcb479c6d60f1914789d
Paul Lockyer <plockyer@googlemail.com>
2024-10-03 16:34:27 +0100

    added the ability to equip an item in a slot

--- main.py
+++ main.py
@@ -9,6 +9,9 @@ location = "start"
 # The players bag for any items they pick up
 player_bag = []
 
+# The slots for a player
+player_slots = { "weapon": None }
+
 # Help menu
 def help():
     print("You can perform the following commands:")
@@ -19,8 +22,33 @@ def help():
     print("l or look - Describe the area you are in")
     print("t or take <item> - Take an item that the area has")
     print("i or inv or inventory - Show the items you have in your bag")
-    print("d or drop <item< - Drop an item to the ground")
+    print("d or drop <item> - Drop an item to the ground")
+    print("e or equip <item> <slot> - Equip an item you have stored in your bag to yourself ( Slots: weapon )")
     print("g or go <direction> - Travel in the direction given ( Can be one of north, south, east or west )")
+
+# Equip an item to the character to be used
+def equip_item(item, slot):
+
+    print(player_slots)
+    print(item)
+    print(slot)
+
+    # Check if wa have the item first
+    if item not in player_bag:
+        print("You can only equip an item you have placed in your bag")
+        return
+    
+    # Check if the slot for the player is available
+    if slot not in player_slots:
+        print("The player does not have a slot matching that description")
+        return
+    
+    # Equip the item
+    player_slots[slot] = item
+
+    # remove the item from your bag
+    player_bag.remove(item)
+    return
 
 # Keep looping until the game is own, lost or quit
 while running:
@@ -75,7 +103,9 @@ while running:
 
                 # Add the item to the list of items the area has
                 world.add_item(location, cmd[1])
+    elif (cmd[0] == "e" or cmd[0] == "equip"):
+        # Equip the item
+        equip_item(cmd[1], cmd[2])
 
-        
     else:
         print("Sorry. That command is not recognised.")
